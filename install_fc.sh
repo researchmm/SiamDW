3390eb8e655b1b6f92886a2639ef41e10930bb32
JudasDie <zpzhang1995@gmail.com>
2019-06-17 17:17:19 +0800

    Add SiamRPN+ training code
    Add SiamRPN+ training code

--- install_fc.sh
+++ install_fc.sh
@@ -10,4 +10,6 @@ conda install pytorch=0.3.1
 pip install torchvision
 pip install opencv-python==3.1.0.5
 pip install numpy==1.12.1
-
+pip install hyperopt
+pip install ray==0.6.3
+pip install requests
--- install_rpn.sh
+++ install_rpn.sh
@@ -1,8 +1,15 @@
 conda install yaml
 conda install shapely
 conda install pyyaml
+pip install scipy
+pip install tensorboardX
+pip install gaft
+pip install mpi4py==2.0.0
 conda install pytorch=0.4.1
+pip install torchvision
 pip install easydict
 pip install opencv-python==3.1.0.5
 pip install numpy==1.12.1
-
+pip install hyperopt
+pip install ray==0.6.3
+pip install requests
